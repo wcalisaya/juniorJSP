7846fed8bdac41ca3ebb92264a09a120f65e4108
wcalisaya <elmagwi@gmail.com>
2015-10-04 02:05:32 -0500

    db

---
+++ magwi.sql
@@ -0,0 +1,39 @@
+/*
+SQLyog Community v8.81 
+MySQL - 5.5.27 : Database - magwi
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`magwi` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `magwi`;
+
+/*Table structure for table `guille` */
+
+DROP TABLE IF EXISTS `guille`;
+
+CREATE TABLE `guille` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(50) DEFAULT NULL,
+  `cargo` varchar(40) DEFAULT NULL,
+  `username` varchar(20) DEFAULT NULL,
+  `pass` varchar(10) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
+
+/*Data for the table `guille` */
+
+insert  into `guille`(`id`,`nombre`,`cargo`,`username`,`pass`) values (1,'William','admin','magwi','123'),(2,'Geovana','admin','geovis','123'),(3,'niquita','nada','niqui','123'),(6,'nasa','ovni','nasareno','123');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
